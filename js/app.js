664aa4e1fa6b8a961330392bfa45ad5f5a3e2623
root <root@DESKTOP-H8O5GSR>
2023-03-03 10:02:53 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+let speed = 100;
+let scale = 0.10;
+let canvas;
+let ctx;
+let logoColor;
+
+let dvd = {
+    x: -1,
+    y: -1,
+    xspeed: 15,
+    yspeed: 9,
+    img: new Image()
+};
+
+(function main(){
+    canvas = document.getElementById("tv-screen");
+    ctx = canvas.getContext("2d");
+    dvd.img.src = 'EasterEgg/annoying-dog-png.png';
+
+    //Draw the "tv screen"
+    canvas.width  = window.innerWidth - 17;
+    canvas.height = window.innerHeight - 19;
+
+    update();
+})();
+
+function update() {
+    setTimeout(() => {
+        //Draw DVD Logo and his background
+        ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        //Move the logo
+        dvd.x+=dvd.xspeed;
+        dvd.y+=dvd.yspeed;
+        //Check for collision 
+        checkHitBox();
+        update();   
+    }, speed)
+}
+
+//Check for border collision
+function checkHitBox(){
+    if (dvd.x + dvd.img.width*scale >= canvas.width || dvd.x <= 0) {
+        dvd.xspeed *= -1;
+    }
+        
+    if (dvd.y + dvd.img.height*scale >= canvas.height || dvd.y <= 0) {
+        dvd.yspeed *= -1;
+    }    
+}
